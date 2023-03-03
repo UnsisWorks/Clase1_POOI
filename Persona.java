5b6814f4e5f82da2be5893c88d2b388ae912a0d7
unsis <jared0192galicia@gmail.com>
2023-03-03 16:02:03 -0600

    Se inicio el contructor de la clase persona

--- Persona.java
+++ Persona.java
@@ -1,14 +1,16 @@
 // Autor: Galicia Cordova Elietzer Jared
 public class Persona () {
 
-	public void Persona() {
-
+	public void Persona(String nombre, String curp, int edad) {
+		this.nombre = nombre;
+		this.curp = curp;
+		this.edad = edad;
 	}
 
 	// Properties
 	String nombre;
 	Stirng curp;
 	int edad;
-	
+
 
 }
