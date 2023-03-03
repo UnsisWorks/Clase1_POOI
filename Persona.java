fc9b0203a0d9c2e212d46a38caa006122f422f42
unsis <jared0192galicia@gmail.com>
2023-03-03 16:11:52 -0600

    Se corrigeron error de compilacion

--- Persona.java
+++ Persona.java
@@ -1,15 +1,19 @@
 // Autor: Galicia Cordova Elietzer Jared
-public class Persona () {
+public class Persona {
 
 	// Properties
 	String name;
-	Stirng curp;
+	String curp;
 	int age;
 
 	public void Persona(String name, String curp, int age) {
 		this.name = name;
 		this.curp = curp;
 		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
