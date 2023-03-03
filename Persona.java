0351ee6c610f74c955b84c10862c547c8524f47b
unsis <jared0192galicia@gmail.com>
2023-03-03 15:57:59 -0600

    Se creo implicitamente el contructor de la case persona

--- Persona.java
+++ Persona.java
@@ -1,8 +1,13 @@
 // Autor: Galicia Cordova Elietzer Jared
 public class Persona () {
-	
+
+	public void Persona() {
+		
+	}
+
 	// Declaracion de variables
 	public static void main (String args[]) {
 
 	}
+
 }
