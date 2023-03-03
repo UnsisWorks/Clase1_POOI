69082b242daaa09d9574dbd688eb9400ab19fd80
unsis <jared0192galicia@gmail.com>
2023-03-03 16:06:29 -0600

    Se modifico el orden de las variables para evitar errores

--- Persona.java
+++ Persona.java
@@ -1,15 +1,15 @@
 // Autor: Galicia Cordova Elietzer Jared
 public class Persona () {
 
+	// Properties
+	String name;
+	Stirng curp;
+	int age;
+
 	public void Persona(String name, String curp, int age) {
 		this.name = name;
 		this.curp = curp;
 		this.age = age;
 	}
-
-	// Properties
-	String name;
-	Stirng curp;
-	int age;
 
 }
